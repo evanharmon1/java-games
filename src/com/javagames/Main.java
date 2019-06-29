5f962e6f5f887c012f431e3409346c63c43f4be5
evanharmon1 <evanharmon1@gmail.com>
2019-06-29 17:52:43 -0500

    Add initial menu system

--- src/com/javagames/Main.java
+++ src/com/javagames/Main.java
@@ -1,8 +1,46 @@
 package com.javagames;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+
+        Scanner in = new Scanner(System.in);
+        Integer selection = 0;
+
+        do {
+            System.out.println("Would you like to play a game?");
+            System.out.println("Java Games");
+            System.out.println("__________");
+            System.out.println("1) Chess");
+            System.out.println("2) Black Jack");
+            System.out.println("");
+            System.out.println("Please enter a selection");
+
+            try {
+                selection = in.nextInt();
+
+                if (selection < 1 || selection > 2) {
+                    System.out.println("Please choose a number between 1 and 2");
+                }
+
+                switch (selection) {
+                    case 1:
+                        System.out.println("Chess is not implemented yet. Remain seated until developer finishes programming the chess game");
+                        selection = 0;
+                        continue;
+                    case 2:
+                        System.out.println("Black Jack is not implemented yet. Please choose another game.");
+                        selection = 0;
+                        continue;
+                }
+
+            } catch (Exception invalid) {
+                System.out.println("Could not handle input. Exiting");
+                System.exit(1);
+            }
+        }
+        while(selection < 1 || selection > 2);
     }
 }
