e338989e324a8f941fcdf6673496b7058d405198
evanharmon1 <evanharmon1@gmail.com>
2019-06-29 22:57:54 -0500

    Implement getting player moves and the game piece

--- src/com/javagames/chess/ChessBoard.java
+++ src/com/javagames/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package com.javagames.chess;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ChessBoard {
@@ -17,12 +19,15 @@ public class ChessBoard {
     public static void displayBoard(){
         for(int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
+                if(j == 0) {
+                    System.out.print(Math.abs(i - 8));
+                }
                 System.out.print(symbols.get(board[i][j]));
             }
             System.out.println("");
         }
 //        for(String pos : board)
-
+        System.out.println(" abcdefgh");
     }
 
     private static HashMap<String, String> symbols = createSymbols();
@@ -39,6 +44,54 @@ public class ChessBoard {
         symbols.put("E", "\u25a0");
 
         return symbols;
+    }
+
+    private static int[] parseInput(String input){
+        String letter = input.substring(0, 1);
+        String number = input.substring(1);
+        int i = 8 - Integer.parseInt(number);
+        int j = 0;
+
+        switch(letter) {
+            case "a":
+                j = 0;
+                break;
+            case "b":
+                j = 1;
+                break;
+            case "c":
+                j = 2;
+                break;
+            case "d":
+                j = 3;
+                break;
+            case "e":
+                j = 4;
+                break;
+            case "f":
+                j = 5;
+                break;
+            case "g":
+                j = 6;
+                break;
+            case "h":
+                j = 7;
+                break;
+        }
+        int[] output = new int[] {i, j};
+        return output;
+    }
+
+    public static void updateBoard(String from, String to){
+        int[] parsedFrom = parseInput(from);
+        int i = parsedFrom[0];
+        int j = parsedFrom[1];
+        String piece = board[i][j];
+
+        int[] parsedTo = parseInput(to);
+
+
+
     }
 
 
--- src/com/javagames/chess/PlayChess.java
+++ src/com/javagames/chess/PlayChess.java
@@ -1,13 +1,32 @@
 package com.javagames.chess;
 
+import java.util.Scanner;
+
 public class PlayChess {
     public static void playChess() {
-        System.out.println("Play Chess");
-        System.out.println("\u2654");
-        System.out.println("\u25a1\u25a0");
-        System.out.println("\u25a0");
 
-        ChessBoard.displayBoard();
+        Scanner in = new Scanner(System.in);
+        String from = "";
+        String to = "";
+
+        do {
+            ChessBoard.displayBoard();
+            try {
+                System.out.println("Select piece you want to move. (e.g. 'b7')");
+                from = in.nextLine();
+                System.out.println("Now select where you want to move that piece. (e.g. 'c7')");
+                to = in.nextLine();
+                System.out.println(from);
+                System.out.println(to);
+                ChessBoard.updateBoard(from, to);
+
+
+            } catch (Exception invalid) {
+                System.out.println("Could not handle input. Exiting");
+                System.exit(1);
+            }
+        }
+        while(true);
 
     }
 }
